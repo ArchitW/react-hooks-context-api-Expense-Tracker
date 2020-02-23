85656e91753018a677f32e6b068e3c99b67bc33a
Archit Warghane <archit2x3@hotmail.com>
2020-02-23 11:51:10 +0530

    [feature] Balance Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
 
 function App() {
 	return (
 		<div>
 			<Header />
+			<div className="container">
+				<Balance />
+			</div>
 		</div>
 	);
 }
